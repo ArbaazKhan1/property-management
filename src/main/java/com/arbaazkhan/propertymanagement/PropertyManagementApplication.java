4a2ac6e7827d350369ebc188be81f86d03efade1
ArbaazKhan1 <bazzy_94@yahoo.com>
2023-08-03 14:58:18 -0500

    Implemented documentation with Swagger and app health monitoring with spring Actuator

--- src/main/java/com/arbaazkhan/propertymanagement/PropertyManagementApplication.java
+++ src/main/java/com/arbaazkhan/propertymanagement/PropertyManagementApplication.java
@@ -2,10 +2,11 @@ package com.arbaazkhan.propertymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2 //This is from springfox dependency and will allow for documentation of our api endpoints
 public class PropertyManagementApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PropertyManagementApplication.class, args);
 	}
